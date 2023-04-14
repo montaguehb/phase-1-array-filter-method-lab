6359852141708afebc7368b15968ead7f1c339b7
Historia Montague <historia.montague@outlook.com>
2023-04-14 17:56:08 -0600

    Finished Lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution here
+
+//would probably be better to just format both strings as either lower or uppercase, used a regexp because I thought it was more interesting that it has a parameter that allows it to be case insensitive.
+const findMatching = (driverArray, name) => driverArray.filter(driver => RegExp(driver, "i").test(name)); 
+
+const fuzzyMatch = (driverArray, name) => driverArray.filter(driver => driver.startsWith(name));
+
+const matchName = (driverArray, name) => driverArray.filter(driver => driver.name.startsWith(name));  
